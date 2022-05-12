929f83441bf98f95ba0a0c04f79d9cb12bf03825
Doinikov Ilia <ilidoin@gmail.com>
2022-05-13 01:49:37 +0300

    Multiline tools added + tests
    EndString, BeginString, FindOpenTags functions moved to tools
    Tests added to *.test.tsx file
    Tests works correctly

--- src/components/math-quill-multyline/math-quill-multyline.tsx
+++ src/components/math-quill-multyline/math-quill-multyline.tsx
@@ -22,6 +22,10 @@ import negIcon from "../../assets/math-symbols/neg.svg";
 import implicIcon from "../../assets/math-symbols/implic.svg";
 import setminusIcon from "../../assets/math-symbols/setminus.svg";
 
+// tools
+
+import { EndString, BeginString, FindOpenTags } from "./math-quill-multyline-tools";
+
 interface MathPair {
   text?: string;
   id?:number;
@@ -115,198 +119,8 @@ const MathQuillMultyline: React.FC<MultylineProps> = ({latex,
     UpdateId();
   };
 
-  const EndString = (text: string) => {
-    let TagList = [];
-    for (let i = 0; i < text.length; i++)
-    {
-      switch (text[i]) {
-        case '{':
-          TagList.push("}");
-          break;
-        case '[':
-          TagList.push("]");
-          break;
-        case '(':
-          TagList.push(")");
-          break;
-        case '}':
-        case ']':
-        case ')':
-          let t = TagList.pop();
-          if (t != text[i])
-          {
-            console.log("Error");
-            console.log(t);
-            console.log(text[i]);
-            console.log(text);
-          }
-          break;
-      }
-    }
-    let out = "";
-    console.log(TagList);
-    for(let i = 0 ; i < TagList.length; i++) {
-      out += TagList[i];
-    }
-    return out;
-  }
-  const BeginString = (text: string) => {
-    let TagList = [];
-    for (let i = text.length - 1; i >= 0; i--)
-    {
-      switch (text[i]) {
-        case '}':
-          TagList.push("{");
-          break;
-        case ']':
-          TagList.push("[");
-          break;
-        case ')':
-          TagList.push("(");
-          break;
-        case '{':
-        case '[':
-        case '(':
-          let t = TagList.pop();
-          if (t != text[i])
-          {
-            console.log("Error");
-            console.log(t);
-            console.log(text[i]);
-            console.log(text)
-          };
-          break;
-      }
-    }
-    let out = "";
-    console.log(TagList);
-    for(let i = 0 ; i < TagList.length; i++) {
-      out += TagList[i];
-    }
-    return TagList;
-  }
-  const FindOpenTags = (tagList: string[], text: string) => {
-    let tagListNew = [] as string[];
-    // Possibility flag
-    let flagPoss = true;
-    let tagsCompleted = 0;
-    for (let i = text.length - 1; i >= 0; i--) {
-      if (tagList.length == 0) {
-        let out = "";
-        console.log(tagListNew);
-        for (let i = 0; i < tagListNew.length; i++) {
-          out += tagListNew[i];
-        }
-        console.log(flagPoss);
-        let L = tagListNew.length;
-        return {out, flagPoss, L};
-      }
-
-      switch (text[i]) {
-        case '}':
-          tagList.push("{");
-          tagsCompleted++;
-          break;
-        case ']':
-          tagList.push("[");
-          tagsCompleted++;
-          break;
-        case ')':
-          tagList.push("(");
-          tagsCompleted++;
-          break;
-        case '{':
-        case '[':
-        case '(':
-          let t = tagList.pop();
-          if (tagsCompleted > 0)
-          {
-            tagsCompleted--;
-            if (t != text[i])
-            {
-              console.log("Error");
-              console.log(t);
-              console.log(text[i]);
-              console.log(text);
-            }
-          }
-          else
-          {
-            let flag = true;
-            if (text[i] == '{')
-            {
-              if (i >= "\\underline".length)
-              {
-                let str = text.slice(i - "\\underline".length, i);
-                console.log(i - "\\underline".length);
-                console.log("\\underline".length);
-                if (str === "\\underline")
-                {
-                  tagListNew.push("\\underline{");
-                  flag = false;
-                }
-                else
-                {
-                  console.log("debug2");
-                  console.log(str);
-                  console.log(text);
-                  console.log(i);
-                }
-              }
-              else {
-                console.log("debug");
-                console.log(text);
-                console.log(i);
-
-              }
-            if (i > "\\textcolor{red}".length && flag)
-              if (text[i - 1] == '}') {
-                let j = i;
-                let s = "";
-                while (text[j] != '\\' && j >= 0) {
-                  s = text[j] + s;
-                  j--;
-                }
-                  if (j == 0 && text[j] != '\\')
-                  {// bad
-                    flagPoss = false;
-                    console.log("bad" + "__" + i.toString());
-                    console.log(text);
-                    console.log(s);
-                  }
-                  else
-                  {
-                    // check correct
-                    console.log("checking");
-                    let s1 = s.slice(0, "textcolor".length);
-                    if (s1 === "textcolor")
-                    {
-                      s = "\\" + s;
-                      tagListNew.push(s);
-                    }
-                    else
-                    {
-                      console.log("different");
-                      console.log(s1);
-                      console.log(s);
-
-                    }
-                  }
-
-              }
-            }
-            else
-            {
-              console.log("bad2" + text + "__" + i.toString());
-              flagPoss = false;
-            }
 
 
-          }
-      }
-    }
-
-  }
   const ClearStr = (text: string) => {
     const SpecialTag = "\\textcolor";
     let t1 = text;
@@ -568,6 +382,8 @@ const MathQuillMultyline: React.FC<MultylineProps> = ({latex,
                           console.log("test1->"+tags1);
                           let l = tags1.length;
                           let tags2 = FindOpenTags(tags1, s0);
+                          console.log("test3")
+                          console.log(s0);
                           console.log(tags2);
 
                           if (tags2 && tags2.out && tags2.flagPoss == true && tags2.L == l) {
